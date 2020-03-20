04163b4b7bd487e6a5cac0bf00cf058219ca79bc
JEAN <56645415+JeanMILPIED@users.noreply.github.com>
2020-03-20 17:47:53 +0100

    more comments; proper SQL connection close; proper SQL Driver;

--- my_python_sql.py
+++ my_python_sql.py
@@ -1,4 +1,4 @@
-#this function checks that user has proper installed and imported packages in its environment
+#check_install() function checks that user has proper installed and imported packages in its environment
 def check_install():
     import importlib
     import subprocess
@@ -9,44 +9,47 @@ def check_install():
     try:
         import pandas as pd
     except ImportError:
-        print("{0} has to be installed".format('pandas'))            
+        print("{0} has to be installed\n".format('pandas'))            
         subprocess.call([sys.executable, '-m', 'pip', 'install', 'pandas'])
     finally:
         import pandas as pd
     try:
         import numpy as np
     except ImportError:
-        print("{0} has to be installed".format('numpy'))            
+        print("{0} has to be installed\n".format('numpy'))            
         subprocess.call([sys.executable, '-m', 'pip', 'install', 'numpy'])
     finally:
         import numpy as np
     try:
         import pyodbc
     except ImportError:
-        print("{0} has to be installed".format('pyodbc'))            
+        print("{0} has to be installed\n".format('pyodbc'))            
         subprocess.call([sys.executable, '-m', 'pip', 'install', 'pyodbc'])
     finally:
         import pyodbc as pyodbc    
-    print("all environment properly constructed")
+    print("Environment has been properly constructed.\n")
 
-#this function connects to the database and manages connection errors    
+#connect_database() function connects to the database and manages connection errors    
 def connect_database(my_server="LAPTOP-1NPS6A7O", my_database="Survey_Sample_A19"):
     global sql_conn #we create a global variable for sql connection
-    #my_string='DRIVER={SQL Server}; SERVER=LAPTOP-1NPS6A7O;DATABASE=Survey_Sample_A19;Trusted_Connection=yes'
-    my_string='DRIVER={SQL Server}; SERVER='+my_server+';DATABASE='+my_database+';Trusted_Connection=yes'
+    
+    pyodbc.pooling = False #this parameter makes the connection to be effectively closed on the server when you delete it
+    
+    my_string='DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+my_server+';DATABASE='+my_database+';Trusted_Connection=yes'
     try:
         sql_conn=pyodbc.connect(my_string)
-        print("connection succeeded.")
+        print("Connection to database {0} on Server {1} succeeded.\n".format(my_database,my_server))
         return sql_conn
+    
     except pyodbc.Error as ex:
         sqlstate = ex.args[0]
         if sqlstate == '28000':
-            print("LDAP Connection failed: check password")
+            print("LDAP Connection failed: check password. \n")
         else:
-            print("connection failed. error is {0} ".format(str(ex.args)))
-        sys.exit('database connection issue') #if no connection possible, we stop the program
+            print("Connection failed. Error is {0} \n".format(str(ex.args)))
+        sys.exit('Database connection issue \n') #if no connection possible, we stop the program
 
-# this function defines a SQL command string to be used in cursor2 to fetch on each survey ID which of the questions are present
+# my_sq_cursor2() function defines a SQL command string to be used in cursor2 to fetch on each survey ID which of the questions are present
 def my_sq_cursor2(currentSurveyId):
     my_string= 'SELECT * FROM ( SELECT SurveyId,QuestionId,1 as InSurvey FROM SurveyStructure WHERE SurveyId ='+str(currentSurveyId)
     my_string=my_string+' UNION SELECT '+str(currentSurveyId) +' as SurveyId, Q.QuestionId, 0 as InSurvey FROM Question as Q'
@@ -54,23 +57,29 @@ def my_sq_cursor2(currentSurveyId):
     my_string=my_string+' AND S.QuestionId = Q.QuestionId)) as t ORDER BY QuestionId'
     return my_string
 
-#this function reports the survey structure based on cursor fetching on the SQL survey database
+#survey_structure() function reports the survey structure based on cursor fetching on the SQL survey database
 def survey_structure(my_sql_connection):
     cursor1=my_sql_connection.cursor()
-    cursor1.execute("SELECT SurveyId FROM Survey ORDER BY SurveyId")
-    my_final_data=[0,0,0]
-    for surveyId in cursor1.fetchall():
-        my_surveyId=surveyId[0]
-        cursor2=my_sql_connection.cursor()
-        cursor2.execute(my_sq_cursor2(my_surveyId))
-        for currentSurveyIdInQuestion, currentQuestionID, currentInSurvey in cursor2.fetchall():
-            my_data=np.array([currentSurveyIdInQuestion, currentQuestionID, currentInSurvey])
-            my_final_data=np.vstack((my_final_data,my_data))
-    my_survey_structure=pd.DataFrame(my_final_data[1:,:]).astype(int)
-    my_survey_structure.columns=['SurveyId','QuestionID','QuestionInSurvey (1=YES,0=NO)']
-    return my_survey_structure
+    try:
+        cursor1.execute("SELECT SurveyId FROM Survey ORDER BY SurveyId")
+        my_final_data=[0,0,0]
+        for surveyId in cursor1.fetchall():
+            my_surveyId=surveyId[0]
+            cursor2=my_sql_connection.cursor()
+            cursor2.execute(my_sq_cursor2(my_surveyId))
+            for currentSurveyIdInQuestion, currentQuestionID, currentInSurvey in cursor2.fetchall():
+                my_data=np.array([currentSurveyIdInQuestion, currentQuestionID, currentInSurvey])
+                my_final_data=np.vstack((my_final_data,my_data))
+        my_survey_structure=pd.DataFrame(my_final_data[1:,:]).astype(int)
+        my_survey_structure.columns=['SurveyId','QuestionID','QuestionInSurvey (1=YES,0=NO)']
+        return my_survey_structure
+    except:
+        sys.exit("Error occured while accessing data. Please check connection information. \n")
+    finally: 
+        cursor1.close()
+        cursor2.close()
 
-# this function builds the SQL string to check questions for each survey and collect the respose data
+# build_strColumnsQueryPart() function builds the SQL string to check questions for each survey and collect the respose data
 def build_strColumnsQueryPart(currentQuestionID, currentInSurvey):
     strQueryTemplateForAnswerColumn = ' COALESCE((SELECT a.Answer_Value FROM Answer as a WHERE a.UserId = u.UserId AND a.SurveyId = <SURVEY_ID> AND a.QuestionId = <QUESTION_ID>), -1) AS ANS_Q<QUESTION_ID> ';
     strQueryTemplateForNullColumnn = ' NULL AS ANS_Q<QUESTION_ID> '
@@ -80,7 +89,7 @@ def build_strColumnsQueryPart(currentQuestionID, currentInSurvey):
         strColumnsQueryPart =strQueryTemplateForAnswerColumn.replace('<QUESTION_ID>',str(currentQuestionID))
     return str(strColumnsQueryPart)
 
-# this function unions the independent queries built for each survey with the build_strColumnsQueryPart() function
+# build_strCurrentUnionQueryBlock() function unions the independent queries built for each survey with the build_strColumnsQueryPart() function
 def build_strCurrentUnionQueryBlock(currentSurveyId,strColumnsQueryPart):
     strQueryTemplateOuterUnionQuery = ' SELECT UserId, <SURVEY_ID> as SurveyId, <DYNAMIC_QUESTION_ANSWERS> FROM [User] as u WHERE EXISTS (SELECT * FROM Answer as a WHERE u.UserId = a.UserId AND a.SurveyId = <SURVEY_ID>)'
     strCurrentUnionQueryBlock = ''
@@ -88,7 +97,7 @@ def build_strCurrentUnionQueryBlock(currentSurveyId,strColumnsQueryPart):
     strCurrentUnionQueryBlock=strCurrentUnionQueryBlock.replace('<SURVEY_ID>', str(currentSurveyId))
     return str(strCurrentUnionQueryBlock)
 
-#this function iterates over the survey structure to build the final query using subfonctions
+#build_final_query() function iterates over the survey structure to build the final query using subfonctions /
 #build_strColumnsQueryPart() and build_strCurrentUnionQueryBlock()
 def build_final_query(my_survey_structure):
     question_id_list=my_survey_structure['QuestionID'].unique()
@@ -114,29 +123,30 @@ def build_final_query(my_survey_structure):
     try:
         with open('./my_query.txt', 'w') as f:
             f.write(strFinalQuery)
-        print("final query saved as 'my_query.txt' in working directory")
+        print("Final query has been saved as 'my_query.txt' in working directory. \n")
     except:
-        sys.exit("error occured in disk saving final query")
+        sys.exit("Error occured while disk saving the final query. \n")
 
 
-#this function checks that a survey structure has already been stored in the working directory and returns it
+#survey_struct_exists() function checks that a survey structure has already been stored in the working directory and returns it /
 # if no survey_structure file exists, it creates and returns it
 def survey_struct_exists(my_sql_connection):
     my_output=1
     my_new_survey=survey_structure(my_sql_connection)
     if path.exists("./last_survey_structure.csv")==True:
-        print("survey structure exists already. let's check if it has changed.")
-        my_old_survey=pd.read_csv("./last_survey_structure.csv",sep=',',index_col=0).astype(int)
+        print("Survey structure file exists already. Let's check if it has changed. \n")
+        my_old_survey=pd.read_csv("./last_survey_structure.csv",sep=',',header=None, skiprows=1,index_col=0).astype(int)
+        my_old_survey.columns=['SurveyId','QuestionID','QuestionInSurvey (1=YES,0=NO)']
         if my_new_survey.equals(my_old_survey)==True:
-            print("the survey structure has not changed")
+            print("The survey structure has not changed. \n")
             my_output=1
         else:
-            print("the survey structure has changed. We build, save and overwrite the old one.")
+            print("The survey structure has changed. We build, save and overwrite the old one. \n")
             my_output=0
             my_new_survey.to_csv('./last_survey_structure.csv', sep=',')
-            print("last_survey_structure.csv properly created in current directory")
+            print("'Last_survey_structure.csv' has been properly created in current directory. \n")
     else:
-        print("there is no survey structure file saved. We have created it")
+        print("There is no survey structure file saved. It has been created and saved. \n")
         my_new_survey.to_csv('./last_survey_structure.csv', sep=',')
         my_output=0
         
@@ -150,26 +160,37 @@ def survey_struct_exists(my_sql_connection):
             my_final_query = file.read().replace('\n', '')     
     return my_final_query
 
+#Main() function orchestrates the job to be performed by calling all usefull functions
 def main(my_Server,my_Database):
-    print("This is main function running")
-    #first we check the packages installed
+    
+    global sql_conn
+    
+    print("\nWelcome. This is main function running. \n")
+    
+    #First we check the packages installed
     check_install()
     
-    #second, we connect to database
+    #Second, we connect to database
     my_conn=connect_database(my_Server, my_Database)
     
-    #third, we check survey structure and get final query (new built or already existing)
+    #Third, we check survey structure and get final query (new built or already existing)
     my_final_query=survey_struct_exists(my_conn)
     
-    #then we make the sql query to get the All survey data and save them on "AllSurveydata.csv" file in working directory
+    #Fourth, we make the sql query to get the All survey data and save them on "AllSurveydata.csv" file in working directory
     df = pd.read_sql(my_final_query, my_conn)
+    df.fillna(value=pd.np.nan, inplace=True)
     df.to_csv("./AllSurveydata.csv")
-    print("database sample hereunder")
-    print(df.head(3))
+    print("Please find the 10 first rows of database hereunder. \n")
+    print(df.head(10),'\n')
     
-    print("job done. 'AllSurveydata.csv' saved in working directory")
+    print("Job completed. 'AllSurveydata.csv' has been saved in working directory. \n")
+    
+    #we close SQL connection and delete it
+    sql_conn.close()
+    del sql_conn
+    print("The connection to the server has been properly closed and deleted. \n")
 
 if __name__ == '__main__':
-    theServer=input("what is the server name ? ")
-    theDatabase=input("what is the database name ? ")
+    theServer=input("What is the SQL SERVER name ? ") #user input interface to get the SERVER name
+    theDatabase=input("What is the SQL DATABASE name ? ")#user input interface to get the DATABASE name
     main(theServer,theDatabase)
